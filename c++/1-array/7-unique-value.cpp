c6a572eecbfb19c0f1930b3eb1032981adbe3f3a
Vijay Tiwari <vijay.tiwari@luxecomforttravels.in>
2026-05-24 13:25:08 +0530

    add unique values finder implementation

---
+++ c++/1-array/7-unique-value.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 2, 2, 3, 4, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Unique values are: ";
+
+    for (int i = 0; i < size; i++) {
+        bool isUnique = true;
+        for (int j = 0; j < i; j++) {
+            if (arr[i] == arr[j]) {
+                isUnique = false;
+                break;
+            }
+        }
+
+        if (isUnique) {
+            cout << arr[i] << " ";
+        }
+    }
+
+    return 0;
+}
