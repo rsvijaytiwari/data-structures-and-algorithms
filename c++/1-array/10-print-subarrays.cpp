ede65bf8b7a1a06fd498de9177537c66d7cc3438
Vijay Tiwari <vijay.tiwari@luxecomforttravels.in>
2026-05-31 13:40:25 +0530

    add total subarrays count in subarray printing implementation

--- c++/1-array/10-print-subarrays.cpp
+++ c++/1-array/10-print-subarrays.cpp
@@ -5,6 +5,8 @@ int main() {
     int arr[] = {5, 2, 8, 1, 9};
     int size = sizeof(arr) / sizeof(arr[0]);
     int pairs = 0;
+    int ts = 0;
+
     for (int i = 0; i < size; i++) {
         int start = i;
         for (int j = 0; j < size; j++) {
@@ -12,8 +14,10 @@ int main() {
             for (int k = start; k <= end; k++) {
                 cout << arr[k] << " ";
             }
+            ts++;
             cout << endl;
         }
     }
+    cout << "Total subarrays: " << ts << endl;
     return 0;
 }
